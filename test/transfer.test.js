8e58ca01d8164b5ccdbaee322bfdd6da20bef833
Andréia Ribeiro <asribeirodesign@gmail.com>
2026-04-06 11:51:30 -0300

    Added tests for GET transfers both total and individual

--- test/transfer.test.js
+++ test/transfer.test.js
@@ -6,11 +6,11 @@ const postTransfers = require("../fixtures/postTransfers.json");
 const { retrieveToken } = require("../helpers/authentication");
 
 describe("transferencias", () => {
+  let token;
+  beforeEach(async () => {
+    token = await retrieveToken("Andreia.Ribeiro", "1234567");
+  });
   describe("POST /transferencias", () => {
-    let token;
-    beforeEach(async () => {
-      token = await retrieveToken("Andreia.Ribeiro", "1234567");
-    });
     it("Should return success with 201 when the transfer amount is equal or above $10", async () => {
       const bodyTranfers = { ...postTransfers };
 
@@ -27,7 +27,7 @@ describe("transferencias", () => {
     it("Should return fail with 422 when the transfer amount is lower than $10", async () => {
       const bodyTranfers = { ...postTransfers };
       bodyTranfers.valor = 7;
-      const response = await request("http://localhost:3000")
+      const response = await request(process.env.BASE_URL)
         .post("/transferencias")
         .set("Content-Type", "application/json")
         .set("Authorization", `Bearer ${token}`)
@@ -36,4 +36,29 @@ describe("transferencias", () => {
       expect(response.status).to.equal(422);
     });
   });
-});
+  describe("GET /transferencias/{id}", () => {
+    it("Should return 200 and the transfer data matching the database record when the ID is valid", async () => {
+      const response = await request(process.env.BASE_URL)
+        .get("/transferencias/40")
+        .set("Authorization", `Bearer ${token}`);
+
+      //console.log(response.status);
+      //console.log(response.body);
+      expect(response.status).to.equal(200);
+      expect(response.body.id).to.equal(40);
+      expect(response.body.id).to.be.a("number");
+      expect(response.body.conta_origem_id).to.equal(1);
+    });
+  });
+  describe("GET /transferencias", () => {
+    it("Should return 10 items when the pagination limit is set to 10", async () => {
+      const response = await request(process.env.BASE_URL)
+        .get("/transferencias?page=1&limit=10")
+        .set("Authorization", `Bearer ${token}`);
+
+      //console.log(response.body);
+      expect(response.status).to.equal(200);
+      expect(response.body.limit).to.equal(10);
+      expect(response.body.transferencias).to.have.lengthOf(10);
+  });
+})
